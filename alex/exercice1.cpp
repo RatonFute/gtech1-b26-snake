42301778ba2b898428956925901dfd52e4bd2613
alex <agouveia@gaming.tech>
2022-01-24 10:47:44 +0100

    try

--- alex/exercice1.cpp
+++ alex/exercice1.cpp
@@ -2,26 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, char** argv){
+int main (int argc, char** argv)
+{
+    SDL_Window* fenetre;
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         fprintf(stdout,"Fail");
         return -1;
     }
-    {
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Test",SDL_WINDOWPOS_UNDEFINED,
-                                        SDL_WINDOWPOS_UNDEFINED,
-                                        640,
-                                        480,
-                                        SDL_WINDOW_SHOWN);
-        if (pWindow){
-            SDL_Delay(3000);
-            SDL_DestroyWindow(pWindow);
-        }
-        else{
-            fprintf(stderr,"Fail 2: %s\n",SDL_GetError());
-        }
+    fenetre = SDL_CreateWindow("Test",SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        640,
+        480,
+        SDL_WINDOW_RESIZABLE);
+    if (fenetre == NULL){
+        printf("Erreur lors de la creation d'une fenetre : %s", SDL_GetError());
+        return EXIT_FAILURE;
     }
+    
+    SDL_Delay(3000);
+    SDL_DestroyWindow(pWindow);
     SDL_Quit();
     return 0;
 }
