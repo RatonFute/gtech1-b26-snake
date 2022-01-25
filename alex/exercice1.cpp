9823e488f9195f18dd001175493273c67eaed09b
alex <agouveia@gaming.tech>
2022-01-25 17:30:59 +0100

    TH

--- alex/exercice1.cpp
+++ alex/exercice1.cpp
@@ -1,34 +1,11 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "exercice1.hpp"
 
 SDL_Window* fenetre;
 SDL_Renderer* renderer;
 
-if(SDL_VideoInit(NULL) < 0)
-{
-    printf("Error %s",SDL_GetError());
-    return EXIT_FAILURE;
-}
-fenetre = SDL_CreateWindowAndRenderer(600 , 600 , SDL_WINDOW_RESIZABLE , &fenetre, &renderer);
-if(fenetre == NULL) // Gestion des erreurs
-{
-    printf("Erreur lors de la creation d'une fenetre : %s",SDL_GetError());
-    return EXIT_FAILURE;
-}
-
-renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Création du renderer
-
-SDL_Delay(10000);
-
-// Destruction du renderer et de la fenêtre :
-SDL_DestroyRenderer(renderer); 
-SDL_DestroyWindow(fenetre);
-SDL_Quit(); // On quitte la SDL
-
-
-/*int main (int argc, char** argv)
+int main (int argc, char** argv)
 {
     SDL_Window* fenetre;
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
@@ -44,9 +21,16 @@ SDL_Quit(); // On quitte la SDL
         printf("Erreur lors de la creation d'une fenetre : %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    
-    SDL_Delay(10000);
+    renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == NULL){
+        printf("Erreur lors de la creation du renderer : %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+    SDL_RenderDrawLine(renderer,0,90,45,120);
+    SDL_Delay(5000);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(fenetre);
     SDL_Quit();
     return 0;
-}*/
+}
