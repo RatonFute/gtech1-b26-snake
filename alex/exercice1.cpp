b00a6fa6a3b10679d7563c60622ef1a14fc64234
alex <agouveia@gaming.tech>
2022-01-25 16:38:42 +0100

    e

--- alex/exercice1.cpp
+++ alex/exercice1.cpp
@@ -11,7 +11,7 @@ if(SDL_VideoInit(NULL) < 0)
     printf("Error %s",SDL_GetError());
     return EXIT_FAILURE;
 }
-fenetre = SDL_CreateWindow("Une fenetre SDL" , SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 600 , 600 , SDL_WINDOW_RESIZABLE);
+fenetre = SDL_CreateWindowAndRenderer(600 , 600 , SDL_WINDOW_RESIZABLE , &fenetre, &renderer);
 if(fenetre == NULL) // Gestion des erreurs
 {
     printf("Erreur lors de la creation d'une fenetre : %s",SDL_GetError());
@@ -20,11 +20,6 @@ if(fenetre == NULL) // Gestion des erreurs
 
 renderer = SDL_CreateRenderer(fenetre, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC); // Création du renderer
 
-if(renderer == NULL)//gestion des erreurs
-{
-    printf("Erreur lors de la creation d'un renderer : %s",SDL_GetError());
-    return EXIT_FAILURE;
-}
 SDL_Delay(10000);
 
 // Destruction du renderer et de la fenêtre :
